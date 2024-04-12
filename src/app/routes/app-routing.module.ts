3555525976ed34024877f986b8dae5c09ddecf0a
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2024-04-12 11:48:45 +0200

    Refaktoryzacja kodu w module AppRoutingModule

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,18 +1,33 @@
-import { AdminGuard } from './../shared/guards/admin.guard';
-import { HomeComponent } from './../home/home.component';
-import { AboutComponent } from './../about/about.component';
-import { NotFoundComponent } from './../not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../shared/guards/auth.guard';
+import { HomeComponent } from './../home/home.component';
+import { AboutComponent } from './../about/about.component';
 import { ForbiddenComponent } from '../forbidden/forbidden.component';
+import { NotFoundComponent } from './../not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'authentication', loadChildren: () => import('../authentication/authentication.module').then(m => m.AuthenticationModule) },
-  { path: 'orders', loadChildren: () => import('../orders/orders.module').then(m => m.OrdersModule), canActivate: [AuthGuard] },
-  { path: 'suppliers', loadChildren: () => import('../suppliers/suppliers.module').then(m => m.SuppliersModule), canActivate: [AuthGuard] },
-  { path: 'tools', loadChildren: () => import('../tools/tools.module').then(m => m.ToolsModule), canActivate: [AuthGuard] },
+  {
+    path: 'authentication',
+    loadChildren: () =>
+      import('../authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('../orders/orders.module').then(m => m.OrdersModule), canActivate: [AuthGuard]
+  },
+  {
+    path: 'suppliers',
+    loadChildren: () =>
+      import('../suppliers/suppliers.module').then(m => m.SuppliersModule), canActivate: [AuthGuard]
+  },
+  {
+    path: 'tools',
+    loadChildren: () =>
+      import('../tools/tools.module').then(m => m.ToolsModule), canActivate: [AuthGuard]
+  },
   { path: 'about', component: AboutComponent },
   { path: 'forbidden', component: ForbiddenComponent },
   { path: '404', component: NotFoundComponent },
@@ -24,4 +39,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
