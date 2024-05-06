8656f9eb5e0533465fe56ded9ae4aa69ae5b1890
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2024-05-06 11:48:35 +0200

    Refaktoryzacja kodu

--- src/app/shared/guards/admin.guard.ts
+++ src/app/shared/guards/admin.guard.ts
@@ -1,22 +1,24 @@
-import { AuthenticationService } from './../services/authentication.service';
 import { Injectable } from '@angular/core';
 import { Router, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from './../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard  {
+export class AdminGuard {
 
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router
-  ) {}
+  ) { }
 
   canActivate(state: RouterStateSnapshot) {
     if (this.authenticationService.isUserAdmin())
       return true;
 
-    this.router.navigate(['/forbidden'], { queryParams: { returnUrl: state.url } });
+    this.router.navigate(['/forbidden'], {
+      queryParams: { returnUrl: state.url }
+    });
     return false;
   }
 }
